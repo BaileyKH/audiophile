7bbe229275b83e84377dff95a59e103813362aec
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2025-01-18 18:34:03 -0700

    Update: Created initial layout for homepage hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,14 @@
 
 export const Home = () => {
     return(
-        <main></main>
+        <main>
+            <section className="bg-[url('/src/assets/home/mobile/image-header.jpg')] sm:bg-[url('/src/assets/home/tablet/image-header.jpg')] lg:bg-[url('/src/assets/home/desktop/image-hero.jpg')] bg-cover bg-center bg-no-repeat w-full h-screen flex justify-center items-center">
+                <div className="flex flex-col justify-center items-center w-full lg:mx-40">
+                    <span>new product</span>
+                    <h1>xx99 mark ii headphones</h1>
+                    <p>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
+                </div>
+            </section>
+        </main>
     );
 }
