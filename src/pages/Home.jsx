dc302abeb4bb4cfafdabcc2058914c4841493fbc
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2025-01-21 16:33:39 -0700

    Update: adjusted text sizing for responsiveness on hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,10 @@ export const Home = () => {
     return(
         <main>
             <section className="bg-[url('/src/assets/home/mobile/image-header.jpg')] sm:bg-[url('/src/assets/home/tablet/image-header.jpg')] lg:bg-[url('/src/assets/home/desktop/image-hero.jpg')] bg-cover bg-center bg-no-repeat w-full h-screen flex justify-center items-center">
-                <div className="flex flex-col justify-center items-center w-full lg:mx-40">
-                    <span>new product</span>
-                    <h1>xx99 mark ii headphones</h1>
-                    <p>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
+                <div className="flex flex-col justify-center items-center lg:items-start space-y-6 text-center lg:text-left w-full px-10 lg:pl-20 sm:px-48 lg:mx-40">
+                    <span className="uppercase text-fontLight/20 font-thin tracking-[10px]">new product</span>
+                    <h1 className="text-fontLight text-4xl sm:text-6xl font-bold uppercase">xx99 mark ii headphones</h1>
+                    <p className="text-fontLight/50 font-light">Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
                 </div>
             </section>
         </main>
